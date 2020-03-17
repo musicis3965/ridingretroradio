a61551c8561377d8b30f8de60db251150a681e4e
musicis3965 <41126317+musicis3965@users.noreply.github.com>
2020-03-17 18:06:13 -0400

    Add files via upload

---
+++ js/merchandise.20200317180120.js
@@ -0,0 +1,31 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+var lwi=-1;function thresholdPassed(){var w=$(window).width();var p=false;var cw=0;if(w>=960){cw++;}if(lwi!=cw){p=true;}lwi=cw;return p;}
+function em1(){var c="sjejohsfuspsbejpAqptu/dpn";var addr="mailto:";for(var i=0;i<c.length;i++)addr+=String.fromCharCode(c.charCodeAt(i)-1);window.location.href=addr;}
+
+$(document).ready(function() {
+r=function(){if(thresholdPassed()){dpi=window.devicePixelRatio;if($(window).width()>=960){var a='data-src'; if($('.js50').hasAttr('src')) { a='src'; } $('.js50').attr(a, 'images/twitter-logo-transparent-back-100.png');
+var a='data-src'; if($('.js51').hasAttr('src')) { a='src'; } $('.js51').attr(a, 'images/youtube-logo-transparent-back-117.png');
+var a='data-src'; if($('.js52').hasAttr('src')) { a='src'; } $('.js52').attr(a, 'images/facebook-logo-transparent-back-110.png');}else{var a='data-src'; if($('.js50').hasAttr('src')) { a='src'; } $('.js50').attr(a, 'images/twitter-logo-transparent-back-90.png');
+var a='data-src'; if($('.js51').hasAttr('src')) { a='src'; } $('.js51').attr(a, 'images/youtube-logo-transparent-back-93.png');
+var a='data-src'; if($('.js52').hasAttr('src')) { a='src'; } $('.js52').attr(a, 'images/facebook-logo-transparent-back-94.png');}}};
+if(!window.HTMLPictureElement){$(window).resize(r);r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+initMenu($('#m4')[0]);
+$('.js50').unveil(50);
+$('.js58 source').unveil(50);
+$('.js51').unveil(50);
+$('.js59 source').unveil(50);
+$('.js52').unveil(50);
+$('.js60 source').unveil(50);
+var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js48')[0], "2.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js49')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js50')[0], "2.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js51')[0], "2.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js52')[0], "2.00s", "0.00s", 1, 100);
+wl.start();
+
+});
