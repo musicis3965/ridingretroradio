6ffd3cf3b566060a2c9bfda4780421285d6de0d0
musicis3965 <41126317+musicis3965@users.noreply.github.com>
2021-02-24 13:59:56 -0500

    Add files via upload

---
+++ js/index.08b4af.js
@@ -0,0 +1,11 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+var wl;
+
+var lwi=-1;function thresholdPassed(){var w=$(window).width();var p=false;var cw=0;if(w>=960){cw++;}if(lwi!=cw){p=true;}lwi=cw;return p;}
+!function(){if("Promise"in window){var e,t,r=document,n=function(){return r.createElement("link")},o=new Set,a=n(),i=a.relList&&a.relList.supports&&a.relList.supports("prefetch"),s=location.href.replace(/#[^#]+$/,"");o.add(s);var c=function(e){var t=location,r="http:",n="https:";if(e&&e.href&&e.origin==t.origin&&[r,n].includes(e.protocol)&&(e.protocol!=r||t.protocol!=n)){var o=e.pathname;if(!(e.hash&&o+e.search==t.pathname+t.search||".html"!=o.substr(-5)&&".html"!=o.substr(-5)&&"/"!=o.substr(-1)))return!0}},u=function(e){var t=e.replace(/#[^#]+$/,"");if(!o.has(t)){if(i){var a=n();a.rel="prefetch",a.href=t,r.head.appendChild(a)}else{var s=new XMLHttpRequest;s.open("GET",t,s.withCredentials=!0),s.send()}o.add(t)}},f=function(e){return e.target.closest("a")},p=function(t){var r=t.relatedTarget;r&&f(t)==r.closest("a")||e&&(clearTimeout(e),e=void 0)},l={capture:!0,passive:!0};r.addEventListener("touchstart",function(e){t=performance.now();var r=f(e);c(r)&&u(r.href)},l),r.addEventListener("mouseover",function(r){if(!(performance.now()-t<1200)){var n=f(r);c(n)&&(n.addEventListener("mouseout",p,{passive:!0}),e=setTimeout(function(){u(n.href),e=void 0},80))}},l)}}();
+
+$(function(){
+r=function(){if(thresholdPassed()){dpi=window.devicePixelRatio;if($(window).width()>=960){var e=document.querySelector('.js1');e.setAttribute('src',(dpi>1)?'images/p1i8qg1tlg5gb7bkf96n_1ro3yguw26uhhx49-766.jpg':'images/p1i8qg1tlg5gb7bkf96n_1ro3yguw26uhhx49-383.jpg');}else{var e=document.querySelector('.js1');e.setAttribute('src',(dpi>1)?'images/p1i8qg1tlg5gb7bkf96n_1ro3yguw26uhhx49-614.jpg':'images/p1i8qg1tlg5gb7bkf96n_1ro3yguw26uhhx49-307.jpg');}}};
+if(!window.HTMLPictureElement){$(window).resize(r);r();}
+
+});
