15cf60a5b612c2f24c21f2714e763dc51c6336bb
musicis3965 <41126317+musicis3965@users.noreply.github.com>
2021-12-20 16:49:56 -0500

    Add files via upload
    tis the season y'all! added a christmas tree photo cause ya know.... festive

---
+++ js/index.5a4b1a.js
@@ -0,0 +1,21 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+var wl;
+
+function em1(){var c="sjejohsfuspsbejpAqptu/dpn";var addr="mailto:";for(var i=0;i<c.length;i++)addr+=String.fromCharCode(c.charCodeAt(i)-1);window.location.href=addr;}
+!function(){if("Promise"in window&&void 0!==window.performance){var e,t,r=document,n=function(){return r.createElement("link")},o=new Set,a=n(),i=a.relList&&a.relList.supports&&a.relList.supports("prefetch"),s=location.href.replace(/#[^#]+$/,"");o.add(s);var c=function(e){var t=location,r="http:",n="https:";if(e&&e.href&&e.origin==t.origin&&[r,n].includes(e.protocol)&&(e.protocol!=r||t.protocol!=n)){var o=e.pathname;if(!(e.hash&&o+e.search==t.pathname+t.search||"?preload=no"==e.search.substr(-11)||".html"!=o.substr(-5)&&".html"!=o.substr(-5)&&"/"!=o.substr(-1)))return!0}},u=function(e){var t=e.replace(/#[^#]+$/,"");if(!o.has(t)){if(i){var a=n();a.rel="prefetch",a.href=t,r.head.appendChild(a)}else{var s=new XMLHttpRequest;s.open("GET",t,s.withCredentials=!0),s.send()}o.add(t)}},p=function(e){return e.target.closest("a")},f=function(t){var r=t.relatedTarget;r&&p(t)==r.closest("a")||e&&(clearTimeout(e),e=void 0)},d={capture:!0,passive:!0};r.addEventListener("touchstart",function(e){t=performance.now();var r=p(e);c(r)&&u(r.href)},d),r.addEventListener("mouseover",function(r){if(!(performance.now()-t<1200)){var n=p(r);c(n)&&(n.addEventListener("mouseout",f,{passive:!0}),e=setTimeout(function(){u(n.href),e=void 0},80))}},d)}}();
+
+$(function(){
+r=function(){dpi=window.devicePixelRatio;var e=document.querySelector('.js1');e.setAttribute('src',(dpi>1)?'images/christmasambiance001-1510.jpg':'images/christmasambiance001-755.jpg');};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+wl=new woolite();
+wl.init();
+wl.addAnimation($('.js2'), "1.80s", "0.00s", 1, 100);
+wl.addAnimation($('.js3'), "2.20s", "0.00s", 1, 100);
+wl.addAnimation($('.js4'), "2.20s", "0.00s", 1, 100);
+wl.addAnimation($('.js5'), "2.20s", "0.00s", 1, 100);
+wl.addAnimation($('.js6'), "2.20s", "0.00s", 1, 100);
+wl.addAnimation($('.js7'), "2.20s", "0.00s", 1, 100);
+wl.start();
+if(location.hash){var o=location.hash.replace('#',''),e=function(){if(document.body.getBoundingClientRect().top>0){var t=document.querySelectorAll('[name="'+o+'"]')[0];t&&t.scrollIntoView(!0),setTimeout(e,100)}};e()}
+});
